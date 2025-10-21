88009e6fb7acee46aa5ef61fbbd1536b1b23769a
TERNOX <43003712+TERNOX@users.noreply.github.com>
2025-10-21 22:58:00 +0000

    Adjust Steam widget for dark theme

--- app/liquid_tags/steam_tag.rb
+++ app/liquid_tags/steam_tag.rb
@@ -26,7 +26,7 @@ class SteamTag < LiquidTagBase
   end
 
   def widget_src
-    "https://store.steampowered.com/widget/#{@app_id}/?l=ukrainian&transparent=1"
+    "https://store.steampowered.com/widget/#{@app_id}/?l=ukrainian&theme=dark&transparent=1"
   end
 end
 
