bb7d9327a58ca50eac2bde55e7beacbbc8497980
TERNOX <ternox1@gmail.com>
2025-10-28 20:33:50 +0200

    Update dashboard.js

--- app/javascript/analytics/dashboard.js
+++ app/javascript/analytics/dashboard.js
@@ -220,7 +220,7 @@ function renderReferrers(data) {
   if (emptyDomainReferrer) {
     tableBody.push(`
       <tr>
-        <td class="align-left">All other external referrers</td>
+        <td class="align-left">Усі інші зовнішні джерела</td>
         <td class="align-right">${emptyDomainReferrer.count}</td>
       </tr>
     `);
