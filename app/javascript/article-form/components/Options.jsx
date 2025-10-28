acf2900a487b713758318a7d3fa4c35416f953f5
TERNOX <43003712+TERNOX@users.noreply.github.com>
2025-10-28 23:30:13 +0000

    Fix scheduled publication time input value

--- app/javascript/article-form/components/Options.jsx
+++ app/javascript/article-form/components/Options.jsx
@@ -113,7 +113,7 @@ export const Options = ({
         <input
           aria-label="Schedule publication time"
           type="time"
-          value="Kyiv"
+          value={publishedAtTime}
           className="crayons-textfield"
           name="publishedAtTime"
           onChange={onConfigChange}
