4fd2d6ad1f946dc34657ad2d0b0273b30780fbb6
TERNOX <43003712+TERNOX@users.noreply.github.com>
2025-11-05 20:37:53 +0000

    Run billboard target org index concurrently

--- db/migrate/20251110120000_add_target_organization_ids_to_display_ads.rb
+++ db/migrate/20251110120000_add_target_organization_ids_to_display_ads.rb
@@ -1,6 +1,8 @@
 class AddTargetOrganizationIdsToDisplayAds < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def change
     add_column :display_ads, :target_organization_ids, :integer, array: true, default: [], null: false
-    add_index :display_ads, :target_organization_ids, using: :gin
+    add_index :display_ads, :target_organization_ids, using: :gin, algorithm: :concurrently
   end
 end
