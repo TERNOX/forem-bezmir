c0cc62894b4e246e96b50c51d28e401946fb769a
Ben Halpern <bendhalpern@gmail.com>
2025-11-20 11:43:24 -0500

    Allow org members to invite via username (#22616)
    * Allow org members to invite via username
    
    * Clean up tests
    
    * Adjust tests
    
    * Clean up some code
    
    * Fix tests
    
    * Fix users
    
    * Fix org count:

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -97,6 +97,7 @@ namespace :admin do
     resources :organizations, only: %i[index show destroy] do
       member do
         patch "update_org_credits"
+        patch "update_fully_trusted"
       end
     end
     resources :emails
