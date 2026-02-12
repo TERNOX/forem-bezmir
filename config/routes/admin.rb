9d846dce26ad61a5d05d5fb7eac031a4594de124
Ben Halpern <bendhalpern@gmail.com>
2026-02-12 12:05:26 -0500

    Add survey builder to admin (internal alpha) (#22765)
    * Merge
    
    * Adjust i18n and show page
    
    * Adjust specs
    
    * Fix spec?
    
    * Poll fix
    
    (cherry picked from commit 1103a2985c7129c07af9ac0a746ee45971a4f39f)

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -134,6 +134,7 @@ namespace :admin do
     resources :tags, only: %i[index new create update edit] do
       resource :moderator, only: %i[create destroy], module: "tags"
     end
+    resources :surveys
   end
 
   scope :customization do
