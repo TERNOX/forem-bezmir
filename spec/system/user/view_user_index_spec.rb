forem/forem

Fix timezone flakiness in profile comments system spec (#23427)

spec/system/user/view_user_index_spec.rb
@@ -76,8 +76,9 @@ RSpec.describe "User index" do
         end
 
         within("#substories .profile-comment-card .profile-comment-row:first-of-type") do
-          comment_date = comment2.readable_publish_date.gsub("  ", " ")
-          expect(page).to have_selector(".comment-date", text: comment_date)
+          comment_date_tz = comment2.readable_publish_date.gsub("  ", " ")
+          comment_date_utc = I18n.l(comment2.created_at.utc, format: :short).gsub("  ", " ")
+          expect(page).to have_selector(".comment-date", text: Regexp.union(comment_date_tz, comment_date_utc))
         end
       end
 

TERNOX/forem-bezmir

(cherry picked from commit 9e946d860108a0f58eda8e99f654382df35c6a45)

spec/system/user/view_user_index_spec.rb
@@ -76,8 +76,9 @@ RSpec.describe "User index" do
         end
 
         within("#substories .profile-comment-card .profile-comment-row:first-of-type") do
-          comment_date = comment.readable_publish_date.gsub("  ", " ")
-          expect(page).to have_selector(".comment-date", text: comment_date)
+          comment_date_tz = comment.readable_publish_date.gsub("  ", " ")
+          comment_date_utc = I18n.l(comment.created_at.utc, format: :short).gsub("  ", " ")
+          expect(page).to have_selector(".comment-date", text: Regexp.union(comment_date_tz, comment_date_utc))
         end
       end
 