c06b367ba3840adffc798119c439a7d472d4184c
TERNOX <ternox1@gmail.com>
2026-09-24 00:53:57 +0300

    Keep the digest switch compatible with priority delivery setup

--- app/workers/emails/send_user_digest_worker.rb
+++ app/workers/emails/send_user_digest_worker.rb
@@ -7,9 +7,9 @@ module Emails
     def perform(user_id, options = {})
       options = options.with_indifferent_access
       attempt = ::EmailDigestTestAttempt.find_by(id: options[:test_attempt_id]) if options[:test_attempt_id]
-      return if !Settings::SMTP.automatic_digests_enabled && attempt.nil?
 
       attempt&.log_event(:info, I18n.t("admin.settings.email_digests_controller.logs.worker_started"), user_id: user_id)
+      return if !Settings::SMTP.automatic_digests_enabled && attempt.nil?
 
       user = User.find_by(id: user_id)
       unless user&.notification_setting&.email_digest_periodic? && user&.registered?
