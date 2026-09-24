0fc268bce7e5c21cc9941f576b1347afb7e053c8
TERNOX <ternox1@gmail.com>
2026-09-24 16:56:07 +0300

    Handle JSON and legacy unsubscribe token payloads

--- app/controllers/email_subscriptions_controller.rb
+++ app/controllers/email_subscriptions_controller.rb
@@ -5,18 +5,41 @@ class EmailSubscriptionsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: %i[unsubscribe]
 
   def unsubscribe
-    verified_params = Rails.application.message_verifier(:unsubscribe).verify(params[:ut])
+    return not_found unless params[:ut].is_a?(String)
 
-    if verified_params[:expires_at] > Time.current
-      user = User.find(verified_params[:user_id])
-      user.notification_setting.update(verified_params[:email_type] => false)
-      @email_type = preferred_email_name.fetch(verified_params[:email_type],
-                                               I18n.t("email_subscriptions_controller.this_list")).call
-    else
-      render "invalid_token"
+    verified_params = Rails.application.message_verifier(:unsubscribe).verify(params[:ut])
+    return render "invalid_token" unless verified_params.is_a?(Hash)
+
+    verified_params = verified_params.with_indifferent_access
+    expires_at = unsubscribe_expiration(verified_params[:expires_at])
+    email_type = verified_params[:email_type].to_s
+    email_name = preferred_email_name[email_type.to_sym]
+    user_id = verified_params[:user_id]
+
+    unless expires_at && expires_at > Time.current && email_name && user_id.to_s.match?(/\A[1-9]\d*\z/)
+      return render "invalid_token"
     end
+
+    user = User.find(user_id)
+    user.notification_setting.update!(email_type => false)
+    @email_type = email_name.call
   rescue ActiveSupport::MessageVerifier::InvalidSignature
     not_found
+  end
+
+  private
+
+  # JSON returns string keys and ISO 8601 timestamps; older Marshal tokens
+  # can contain symbol keys and Time/TimeWithZone values.
+  def unsubscribe_expiration(value)
+    case value
+    when String
+      Time.iso8601(value)
+    when Time, DateTime, ActiveSupport::TimeWithZone
+      value.to_time
+    end
+  rescue ArgumentError
+    nil
   end
 
   def preferred_email_name
